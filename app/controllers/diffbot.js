1c3325fb08598fccbccdab7889dee3968a75b42d
Andy Jiang <andyjiang@gmail.com>
2014-03-20 17:30:50 -0700

    error handling on diffbot

--- app/controllers/diffbot.js
+++ app/controllers/diffbot.js
@@ -7,21 +7,40 @@ var request = require('request');
 module.exports = {
 	// Get JSON.
 	getListings: function getListings (obj, callback) {
+		if (typeof obj === 'function') {
+			callback = obj;
+			obj = {
+				keywords: [],
+				neighborhoods: []
+			};
+		}
+
 		// Obj.
 		// .keywords: [w4m, m4w, white, beard, bearded, coffee, ..]
 		// .neighborhoods: [48, 112, ..]
-		// .ageRange: [22, 35]
+		// .maxAge: 35
+		// .minAge: 20
 
 		var keywords = obj.keywords.join('+');
-		var minAge = Math.min.apply(null, obj.ageRange);
-		var maxAge = Math.max.apply(null, obj.ageRange);
-		var neighborhoods = 'nh=' + obj.neighborhoods.join('&nh=') + '&';
+		var minAge = '';
+		var maxAge = '';
+
+		if (obj.minAge)
+			minAge = obj.minAge
+
+		if (obj.maxAge)
+			maxAge = obj.maxAge
+
+		var neighborhoods = '';
+		if (obj.neighborhoods.length > 0)
+			neighborhoods = 'nh=' + obj.neighborhoods.join('&nh=') + '&';
 
 		// Need to set the URL.
 		var craigslistUrl = 'http://sfbay.craigslist.org/search/mis?zoomToPosting=&catAbb=mis&query=' +
 												keywords + 
-												'&minAsk=' + minAge
-												'&maxAsk=' + maxAge
+												'&minAsk=' + minAge +
+												'&maxAsk=' + maxAge +
+												neighborhoods +
 												'&excats=' +
 												'';
 
