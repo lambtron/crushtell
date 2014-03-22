882125eaf1062b88bbc68cdc1fd55cee46afe7af
Andy Jiang <andyjiang@gmail.com>
2014-03-22 08:20:56 -0700

    added angular

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -5,5 +5,24 @@ crushtell.controller('mainController',
 	function ($scope, $http)
 {
 	// Setting up variables needed for alerts and notification system.
-	
+	var user = $scope.user = {
+		name: '',
+		email: '',
+		type: [],											// w4m, m4m
+		keywords: [],
+		location: [],
+		neighborhoods: [],
+		submit: function submit() {
+			$http.post('/setup', user)
+			.success(function (data) {
+				console.log(data);
+			})
+			.error(function (data) {
+				console.log('Error: ');
+				console.log(data);
+			});
+		}
+	};
+
+
 }]);
---
+++ worker.js
@@ -0,0 +1,3 @@
+// Retrieve an array of people from MongoDB.
+// For each person, run diffbot and see if there are any new posts for that day
+// Send an email to that person.
